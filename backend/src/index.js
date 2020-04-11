2cc79657f63091cfa5b095c187e79cd8b3dae3d1
bsicchieri <brunosicchieri@hotmail.com>
2020-04-11 00:01:25 -0300

    backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,3 +3,15 @@ const express = require('express');
 
 // declarando a varival app
 const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World' });
+});
+
+// porta
+app.listen(3333, () => {
+  console.log('Back-end started!');
+});
+
+
+

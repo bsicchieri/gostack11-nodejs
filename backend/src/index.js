b22eba54e02893bc7748d9ccdc89472e99870639
bsicchieri <brunosicchieri@hotmail.com>
2020-04-11 12:49:21 -0300

    backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,8 +4,15 @@ const express = require('express');
 // declarando a varival app
 const app = express();
 
+app.use(express.json());
+
 // metodo GET
 app.get('/projects', (request, response) => {
+  const { title, owner } = request.query;
+
+  console.log(title);
+  console.log(owner);
+
   return response.json([
     'Projeto 1',
     'Projeto 2',
@@ -14,6 +21,11 @@ app.get('/projects', (request, response) => {
 
 // metodo POST
 app.post('/projects', (request, response) => {
+  const { title, owner } = request.body;
+  
+  console.log(title);
+  console.log(owner);
+  
   return response.json([
     'Projeto 1',
     'Projeto 2',
@@ -23,6 +35,10 @@ app.post('/projects', (request, response) => {
 
 // metodo PUT
 app.put('/projects/:id', (request, response) => {
+  const { id } = request.params;
+
+  console.log(id);
+
   return response.json([
     'Projeto 4',
     'Projeto 2',
@@ -32,6 +48,10 @@ app.put('/projects/:id', (request, response) => {
 
 // metodo DELETE
 app.delete('/projects/:id', (request, response) => {
+  const { id } = request.params;
+
+  console.log(id);
+
   return response.json([
     'Projeto 2',
     'Projeto 3',

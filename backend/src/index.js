0a8a2c1fd3f93ba1db47341db37653e8f8060456
bsicchieri <brunosicchieri@hotmail.com>
2020-04-11 13:29:29 -0300

    backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,12 +11,13 @@ const projects = [];
 
 // metodo GET
 app.get('/projects', (request, response) => {
-  // const { title, owner } = request.query;
+  const { title } = request.query;
 
-  // console.log(title);
-  // console.log(owner);
+  const results = title
+    ? projects.filter(project => project.title.includes(title))
+    : projects;
 
-  return response.json(projects);
+  return response.json(results);
 });
 
 // metodo POST
@@ -33,26 +34,38 @@ app.post('/projects', (request, response) => {
 // metodo PUT
 app.put('/projects/:id', (request, response) => {
   const { id } = request.params;
+  const { title, owner } = request.body;
 
-  console.log(id);
+  const projectIndex = projects.findIndex(project => project.id === id);
 
-  return response.json([
-    'Projeto 4',
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found!' });
+  }
+
+  const project = {
+    id,
+    title,
+    owner,
+  };
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
 });
 
 // metodo DELETE
 app.delete('/projects/:id', (request, response) => {
   const { id } = request.params;
 
-  console.log(id);
+  const projectIndex = projects.findIndex(project => project.id === id);
 
-  return response.json([
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found!' });
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).jsonsend();
 });
 
 // porta
